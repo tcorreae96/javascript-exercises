1eb6178be1083764d4b587d5aceece6cabd9cfd8
Tomas Correa <79479585+tcorreae96@users.noreply.github.com>
2024-07-31 13:31:36 -0400

    finished exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,9 +1,25 @@
 const fibonacci = function(num) {
-	const fibonacciSeq = [];
-	for (let i = 0; i < num; i++) {
-		
+	if (num < 0) {
+		return "OOPS";
+	} else if (num === 0) {
+		return 0;
+	} else if (num === 1 || num == 2) {
+		return 1;
+	} else {
+		return calculateNum(num);
 	}
 };
+
+function calculateNum(num) {
+	let seq = [0, 1, 1];
+	// The sequence is the sum of the last 2 numbers.
+	for (let i = 3; i <= num; i++) {
+		let newNumber = seq[i - 1] + seq[i - 2];
+		seq.push(newNumber);
+	}
+	return seq[num];
+
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
