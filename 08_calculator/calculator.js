0afdc08708e6f33d5758f0afd16b55f606789b0e
Tomas Correa <79479585+tcorreae96@users.noreply.github.com>
2024-07-30 20:05:45 -0400

    finish exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((sum, number) => sum + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+	return numbers.reduce((mult, number) => mult * number, 1);
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	let total = 1;
+	for (let i = 0; i < pow; i++) {
+		total *= num;
+	}
+	return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1;
+	for (num; 0 < num; num--) {
+		total *= num;
+	}
+	return total;
 };
 
 // Do not edit below this line
