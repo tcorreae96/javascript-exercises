f94614fee358b6c5e5c676b346adeaec34edea57
Tomas Correa <79479585+tcorreae96@users.noreply.github.com>
2024-07-30 20:25:34 -0400

    finished exercise 9 shortened

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,11 +1,7 @@
 const palindromes = function (str) {
-	const newStr = str.replaceAll(/([^a-zA-Z0-9])|(_)/g, '').toLowerCase();
+	const newStr = str.toLowerCase().replaceAll(/([^a-z0-9])|(_)/g, '');
 	const reversedNewStr = newStr.split("").reverse().join("");
-	if (newStr === reversedNewStr) {
-		return true;
-	} else {
-		return false;
-	}
+	return newStr === reversedNewStr;
 };
 
 // Do not edit below this line
