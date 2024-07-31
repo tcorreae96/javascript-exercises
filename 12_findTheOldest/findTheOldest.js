b4e1144531468b2390909a3a4be2ce40423f08ff
Tomas Correa <79479585+tcorreae96@users.noreply.github.com>
2024-07-31 14:03:33 -0400

    finished exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+	/* 	
+	1. Loop through all the people (map, filter, reduce)
+	2. Order the people from oldest to youngest (sort)
+	3. Return the name of the first person on that list.
+	*/
 
+	const today = new Date().getFullYear();
+	
+	return (people
+		.filter(person => person.yearOfBirth && person.yearOfDeath ? person.yearOfDeath : person.yearOfDeath = today)
+		.sort((a,b) => (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth))[0]
+	);
 };
 
 // Do not edit below this line
