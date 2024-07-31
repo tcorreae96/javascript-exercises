13870b0e210f0986efae4b4ba1a23cab532067d4
Tomas Correa <79479585+tcorreae96@users.noreply.github.com>
2024-07-30 20:24:24 -0400

    finished exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+	const newStr = str.replaceAll(/([^a-zA-Z0-9])|(_)/g, '').toLowerCase();
+	const reversedNewStr = newStr.split("").reverse().join("");
+	if (newStr === reversedNewStr) {
+		return true;
+	} else {
+		return false;
+	}
 };
 
 // Do not edit below this line
